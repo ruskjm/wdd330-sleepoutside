45ad68d39693a5aa6a27abb279db2af28a3c295f
ruskjm <rus23008@byui.edu>
2024-09-23 18:43:16 -0700

    push out updates

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,46 +1,37 @@
-import {
-    renderListWithTemplate
-  } from "./utils.mjs";
-  
-  function productCardTemplate(product) {
-    return `<li class="product-card">
-    <a href="product_pages/index.html?product=${product.Id}">
-    <img
-      src="${product.Image}"
-      alt="Image of ${product.Name}"
-    />
-    <h3 class="card__brand">${product.Brand.Name}</h3>
-    <h2 class="card__name">${product.Name}</h2>
-    <p class="product-card__price">$${product.FinalPrice}</p></a>
-  </li>`;
-  }
-  
-  export default class ProductListing {
-    constructor(category, dataSource, listElement) {
-        // We passed in this information to make our class as reusable as possible.
-        // Being able to define these things when we use the class will make it very flexible
-        this.category = category;
-        this.dataSource = dataSource;
-        this.listElement = listElement;
-    }
-  
-    async init() {
-        // our dataSource will return a Promise...so we can use await to resolve it.
-        const list = await this.dataSource.getData();
-        // render the list - to be completed
-        this.renderList(list);
-    }
+import { renderListWithTemplate } from "./utils.mjs";
 
-      
-    // render before doing the stretch
-    // renderList(list) {
-    //   const htmlStrings = list.map(productCardTemplate);
-    //   this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
-    // }
-  
-    // render after doing the first stretch
-    renderList(list) {
-        renderListWithTemplate(productCardTemplate, this.listElement, list);
-    }
-    
+function productCardTemplate(product) {
+  return `<li class="product-card">
+  <a href="product_pages/index.html?product=${product.Id}">
+  <img
+    src="${product.Image}"
+    alt="Image of ${product.Name}"
+  />
+  <h3 class="card__brand">${product.Brand.Name}</h3>
+  <h2 class="card__name">${product.Name}</h2>
+  <p class="product-card__price">$${product.FinalPrice}</p></a>
+</li>`;
+}
+
+export default class ProductListing {
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
   }
+
+  async init() {
+    const list = await this.dataSource.getData();
+    const filteredList = this.filterProducts(list);
+    this.renderList(filteredList);
+  }
+
+  filterProducts(list) {
+    // Return only the first four items from the list
+    return list.slice(0, 4);
+  }
+
+  renderList(list) {
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+  }
+}
