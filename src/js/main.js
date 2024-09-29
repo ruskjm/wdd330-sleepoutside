5f654619e1052d93d5f7cd1a8ce8b9a4b1e0397e
ruskjm <rus23008@byui.edu>
2024-09-29 14:21:38 -0700

    add superscript number

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,3 @@
-// import ProductData from "./ProductData.mjs";
-// import ProductList from "./ProductList.mjs";
-
-// const dataSource = new ProductData("tents");
-// const listElement = document.querySelector(".product-list");
-// const productList = new ProductList("Tents", dataSource, listElement);
-
-// productList.init();
-
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { setupCartIcon, updateCartIcon } from "./cartIcon.js";
